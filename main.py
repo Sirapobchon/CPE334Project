7d4f894869721830d87f40641978db7384db2414
Sirapob Chonphanarak <76686829+Sirapobchon@users.noreply.github.com>
2023-10-07 21:21:57 +0700

    REST framework and admin user creation

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ import flet as ft
 import requests
 
 def main(page: ft.Page):
-    page.title = "Flet Page"
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.title = "Flet Main Page"
+    #page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    
+
+
+    page.add(
+
+    )
 
 ft.app(target=main)
