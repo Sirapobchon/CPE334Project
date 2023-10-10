691ca546174430a772d935bcd1ef28df4764019b
Sirapob Chonphanarak <76686829+Sirapobchon@users.noreply.github.com>
2023-10-10 23:06:00 +0700

    Signinup Refactor

--- fletapp/signinup.py
+++ fletapp/signinup.py
@@ -1,26 +1,24 @@
-import flet
-from flet import *
+import flet as ft
 import math
 
-def main(page : Page):
+def main(page : ft.Page):
+    #page.theme_mode = ft.ThemeMode.LIGHT
     page.title="Login and Register"
-    page.bgcolor="black"
 
-    login = Container(
-        width=320,
-        height=750,
+    login = ft.SafeArea(ft.Container(
         #image_src="signinbg.jpg",
         
         #image_fit=ImageFit.NONE,
         #expand=True,
-        gradient=LinearGradient(
+        alignment=ft.alignment.center,
+        gradient=ft.LinearGradient(
             rotation=math.pi / 3,
             colors=["#86e3ce","#d0e6a5","#ffde95","#fa897b","#ccabd8"],
             ),
 
         #bgcolor="#ffffff",
         border_radius=10,
-        content=Column(
+        content=ft.Column(
             width = 320,
             controls=[
                 #Container(
@@ -28,20 +26,20 @@ def main(page : Page):
                         #src=signinbg.jpg"
                     #)
                 #),
-                Container(
+                ft.Container(
                     width=300,
-                    margin=margin.only(left=170,right=10,top=10),
-                    content=TextButton(
+                    margin=ft.margin.only(left=170,right=10,top=10),
+                    content=ft.TextButton(
                         "Create Account",
-                        style=ButtonStyle(
+                        style=ft.ButtonStyle(
                             color="#7D7C7C"
                         )
                     )
                 ),
-                Container(
+                ft.Container(
                     width =300,
-                    margin=margin.only(left=90,right=10,top=20),
-                    content=Text(
+                    margin=ft.margin.only(left=90,right=10,top=20),
+                    content=ft.Text(
                         "WELCOME",
                         #text_align="center",
                         size=24,
@@ -49,52 +47,58 @@ def main(page : Page):
                         weight="w700"
                     )
                 ),
-                Container(
+                ft.Container(
                     width =300,
-                    margin=margin.only(left=100,right=10,top=5),
-                    content=Text(
+                    margin=ft.margin.only(left=100,right=10,top=5),
+                    content=ft.Text(
                         "LifeHack!",
                         size=24,
                         color="#000000",
                         weight="w700"
                     )
                 ),
-                Container(
+                ft.Container(
                  width =300,
-                    margin=margin.only(left=65,right=10,top=20),
-                    content=Text(
+                    margin=ft.margin.only(left=65,right=10,top=20),
+                    content=ft.Text(
                         "Hack your life by lifeHack",
                         #text_align="center",
                         size=15,
                         color="#000000"   
                     )
                 ),
-                Container(
-                  width =300,  
-                  margin=margin.only(left=10,right=10,top=20), 
-                  content=Column(
-                      controls=[
-                            TextField(
+                ft.Container(
+                    theme=ft.Theme(color_scheme_seed=ft.colors.BLACK),
+                    theme_mode=ft.ThemeMode.LIGHT,
+                    width=300,  
+                    margin=ft.margin.only(left=10,right=10,top=20), 
+                    content=ft.Column(
+                        controls=[
+                            ft.TextField(
+                                
                                 label="User Name or E-mail",
-                                text_style = TextStyle(
+                                
+                                text_style = ft.TextStyle(
                                     size=14,
                                     color="#000000",
                                 ),
                                 border_radius=40,
-                                border_color=colors.BLACK,
-                                focused_border_color=colors.ORANGE_700,
+                                border_color=ft.colors.BLACK,
+                                focused_border_color=ft.colors.ORANGE_700,
                             )
                         ]
                     )
                 ),
-                Container(
-                  width =300,  
-                  margin=margin.only(left=10,right=10,top=5), 
-                  content=Column(
-                      controls=[
-                            TextField(
+                ft.Container(
+                    theme=ft.Theme(color_scheme_seed=ft.colors.BLACK),
+                    theme_mode=ft.ThemeMode.LIGHT,
+                    width =300,  
+                    margin=ft.margin.only(left=10,right=10,top=5), 
+                    content=ft.Column(
+                        controls=[
+                            ft.TextField(
                                 label="Password",
-                                text_style = TextStyle(
+                                text_style = ft.TextStyle(
                                     size=14,
                                     color="#000000",
                                 ),
@@ -102,32 +106,32 @@ def main(page : Page):
                                 can_reveal_password=True,
                                 
                                 border_radius=40,
-                                border_color=colors.BLACK,
-                                focused_border_color=colors.ORANGE_700,
+                                border_color=ft.colors.BLACK,
+                                focused_border_color=ft.colors.ORANGE_700,
                             )
                         ]
                     )
                 ),
-                Container(
+                ft.Container(
                     width=300,
-                    margin=margin.only(left=150,right=10,top=5), 
-                    content=TextButton(
+                    margin=ft.margin.only(left=150,right=10,top=5), 
+                    content=ft.TextButton(
                         "Forgot Password?" ,
-                        style=ButtonStyle(
+                        style=ft.ButtonStyle(
 
                         )
                     )
                 ),
-                Container(
+                ft.Container(
                     width=300,
-                    margin=margin.only(left=20,right=10,top=5), 
-                    content=TextButton(
+                    margin=ft.margin.only(left=20,right=10,top=5,bottom=20), 
+                    content=ft.TextButton(
                         "Login" ,
                         width=300,
                         height=50,
-                        style=ButtonStyle(
+                        style=ft.ButtonStyle(
                             color="#ffffff",
-                            bgcolor=colors.ORANGE_700,
+                            bgcolor=ft.colors.ORANGE_700,
                             shape={}
 
                         )
@@ -135,38 +139,10 @@ def main(page : Page):
                     )
                 )
             ]
-
-
-
-        
-
-        ),
-
-    )
-    signup = Container(
-        width=320,
-        height=750,
-        bgcolor="#ffffff",
-        border_radius=10,
-        content=Column(
-        
-
         ),
     )
-
-
-    body = Container(
-        height=800,
-        content=Row(
-            controls=[
-                login,
-                signup,
-
-            ]
-        ),
-        #padding=padding.only(left=300)
-
     )
-    page.add(body)
 
-app(target=main)
+    page.add(login)
+
+ft.app(target=main)
