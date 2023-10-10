305569da672741ab430a7763ef0cda9a44b96fb7
Sirapob Chonphanarak <76686829+Sirapobchon@users.noreply.github.com>
2023-10-10 20:50:00 +0700

    Flet App to android and ios

--- fletapp/main.py
+++ fletapp/main.py
@@ -4,11 +4,11 @@ import requests
 def main(page: ft.Page):
     page.title = "Flet Main Page"
     #page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    mainsafe = ft.SafeArea(ft.Text("Hello, Flet!"))
     
-
-
     page.add(
-
+        mainsafe
     )
+
 
 ft.app(target=main)
