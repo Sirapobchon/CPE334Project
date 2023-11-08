8e91a485c17c973b552766e58af5ce137b782ea5
Sirapob Chonphanarak <76686829+Sirapobchon@users.noreply.github.com>
2023-11-08 17:54:50 +0700

    New route

--- fletapp/main.py
+++ fletapp/main.py
@@ -9,26 +9,16 @@ def main(page: ft.Page):
     }
     
     #defining the window size
-    page.window_max_width = 410
-    page.window_width = 800
-    page.window_max_height = 800
-    page.window_height = 800
+    page.window_min_width = 420
+    page.window_width = 420
+    page.window_min_height = 820
+    page.window_height = 820
 
     page.title = "Project 334 Appication"
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.scroll = ft.ScrollMode.HIDDEN
     page.padding = 0
     page.bgcolor = "#ddf7f1"
-
-    page.navigation_bar = ft.NavigationBar(bgcolor = "#fe96a5",selected_index=2,
-        destinations=[
-            ft.NavigationDestination(icon=ft.icons.CHECK, ),
-            ft.NavigationDestination(icon=ft.icons.SHOPPING_BAG, ),
-            ft.NavigationDestination(icon=ft.icons.HOME, ),
-            ft.NavigationDestination(icon=ft.icons.CALCULATE, ),
-            ft.NavigationDestination(icon=ft.icons.PERSON, ),
-        ]
-    )
 
     def route_change(route):
         print(page.route)
@@ -37,7 +27,6 @@ def main(page: ft.Page):
         page.views.append(
             views_handle.views(page)[page.route],
         )
-        page.navigation_bar.selected_index = 2
         page.update()
             
     page.on_route_change = route_change
