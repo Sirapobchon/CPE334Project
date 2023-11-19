8dbfcd5821324d02bfdf6bb9aebe4845015e1d6c
DevNebulaP <76225632+DevNebulaP@users.noreply.github.com>
2023-11-20 05:11:57 +0700

    Stretchy

--- fletapp/views.py
+++ fletapp/views.py
@@ -1,4 +1,5 @@
 from flet import *
+import flet as ft
 from homepage import Home
 #from todo import main
 from tobuy_new import ToBuyMain
@@ -6,6 +7,33 @@ from calculator_new2 import Calculator
 from signup import SignupMain
 from login import LoginMain
 from forgetpass import ForgetMain
+
+class ChangeNav(ft.UserControl):
+    def __init__(self, page, selected_index):
+        super().__init__()
+        self.page = page
+        self.index = selected_index
+
+    def changetab(self):
+        destinations = ['/', '/tobuy', '/', '/calculator', '/login']
+        destination_url = destinations[self.index]
+        self.page.go(destination_url)
+
+    def build(self):
+        print(self.index)  # Note: This print statement should be inside the build method
+        return None  # You need to return something from the build method, even if it's None
+
+def navBar(selectedIndex):
+    return ft.NavigationBar(bgcolor="#fe96a5", selected_index=selectedIndex,
+        destinations=[
+            ft.NavigationDestination(icon=ft.icons.CHECK),
+            ft.NavigationDestination(icon=ft.icons.SHOPPING_BAG),
+            ft.NavigationDestination(icon=ft.icons.HOME),
+            ft.NavigationDestination(icon=ft.icons.CALCULATE),
+            ft.NavigationDestination(icon=ft.icons.PERSON),
+        ],
+        on_change=lambda e: ChangeNav(e.page, e.control.selected_index).changetab(),
+    )
 
 def views(page):
     return {
@@ -32,7 +60,8 @@ def views(page):
             route='/calculator/',
             controls=[
                 Calculator(page)
-            ]
+            ],
+            navigation_bar=navBar(3)
         ),
         '/signup': View(
             route='/signup/',
